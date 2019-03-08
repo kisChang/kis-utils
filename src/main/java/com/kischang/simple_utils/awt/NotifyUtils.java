00611491481de35f6c97193c6973f2ff565fa70f
KisChang_Win10 <734615869@qq.com>
2019-03-08 15:24:49 +0800

    完善NotifyUtils

--- src/main/java/com/kischang/simple_utils/awt/NotifyUtils.java
+++ src/main/java/com/kischang/simple_utils/awt/NotifyUtils.java
@@ -13,19 +13,23 @@ import java.awt.*;
 public class NotifyUtils {
 
     public static void displayMessage(String msg) {
-        displayMessage(msg, TrayIcon.MessageType.INFO);
+        displayMessage(msg, 2000);
     }
 
-    public static void displayMessage(String msg, TrayIcon.MessageType messageType) {
-        displayMessage("提示", msg, messageType);
+    public static void displayMessage(String msg, long time) {
+        displayMessage(msg, time, TrayIcon.MessageType.INFO);
     }
 
-    public static void displayMessage(String title, String msg, TrayIcon.MessageType messageType) {
+    public static void displayMessage(String msg, long time, TrayIcon.MessageType messageType) {
+        displayMessage("提示", msg, time, messageType);
+    }
+
+    public static void displayMessage(String title, String msg, long time, TrayIcon.MessageType messageType) {
         if(OS.isFamilyUnix()){
             System.out.println("MSG:" + msg);
         }
         if (Desktop.isDesktopSupported()){
-            ToastMessage.mk(msg).display();
+            ToastMessage.mk(msg, time).display();
         }
     }
 
