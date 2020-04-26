922f1b0203c2d1fad0940b5679cdea9dc723a597
KisChang <734615869@qq.com>
2020-04-26 17:58:14 +0800

    完善SimpleDateConverter对不同类型、空时间的处理

--- src/main/java/com/kischang/simple_utils/web/converter/SimpleDateConverter.java
+++ src/main/java/com/kischang/simple_utils/web/converter/SimpleDateConverter.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.convert.converter.Converter;
 
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -15,13 +16,34 @@ public class SimpleDateConverter implements Converter<String, Date> {
 
     private static Logger logger = LoggerFactory.getLogger(SimpleDateConverter.class);
     private static final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+    private static final SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss");
 
     @Override
     public Date convert(String source) {
-        try {
-            return simpleDateFormat.parse(source);
-        } catch (ParseException e) {
-            logger.warn("parse date fail, source: {}", source, e);
+        if (source == null
+                || "".equals(source)
+                || "null".equals(source)) {
+            return null;
+        }
+
+        if (source.length() >= 19){
+            try {
+                return simpleDateFormat.parse(source);
+            } catch (ParseException e) {
+                logger.debug("parse date [] fail, source: {}", source, e);
+            }
+        }else {
+            try {
+                return dateFormat.parse(source);
+            }catch (ParseException e){
+                logger.debug("parse date fail, source: {}", source, e);
+                try {
+                    return timeFormat.parse(source);
+                } catch (ParseException e1) {
+                    logger.debug("parse date fail, source: {}", source, e1);
+                }
+            }
         }
         return null;
     }
